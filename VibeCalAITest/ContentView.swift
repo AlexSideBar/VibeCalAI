71209e5d470acef9dbe076198f0cb7d4d5af4f55
Aditya Saravana <aditya.saravana@icloud.com>
2025-06-16 12:00:32 -0700

    Initial Commit

---
+++ VibeCalAITest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VibeCalAITest
+//
+//  Created by Aditya Saravana on 6/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VibeCalAITest/VibeCalAITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCalAITestApp.swift
+//  VibeCalAITest
+//
+//  Created by Aditya Saravana on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct VibeCalAITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
